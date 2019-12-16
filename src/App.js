860095f3c5122f786cf468374b042f7acb1fba6b
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2019-12-16 10:08:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+
+state = {
+  apiResponse:[]
+}
+
+componentDidMount = () => {
+  axios.get('https://test.wertkt.com/api/biz/')
+  .then(response => {
+    this.setState( { apiResponse: response.data } )
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TODO-List</h1>
+        {
+          this.state.apiResponse.map(apiResponse =>(
+            <div key={apiResponse.id}>
+                <input type="checkbox" id="todo" name="todo" />
+                <label for="todo">{apiResponse.name}</label>
+            </div>
+          ))
+        }
+        
       </header>
     </div>
   );
+}
 }
 
 export default App;
